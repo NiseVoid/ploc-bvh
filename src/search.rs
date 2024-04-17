ac37438d79c5fdbf6358769ba559abf887a43747
NiseVoid <nisevoid@gmail.com>
2024-04-17 03:31:07 +0200

    Make unsafe code for find cache less sketchy

--- src/search.rs
+++ src/search.rs
@@ -2,26 +2,43 @@ use crate::{BvhNode, BvhVolume};
 
 const SEARCH_RADIUS: usize = 14;
 
-#[derive(Default)]
-pub struct FindCache([[f32; SEARCH_RADIUS]; SEARCH_RADIUS]);
+mod cache {
+    use super::SEARCH_RADIUS;
 
-impl FindCache {
-    #[inline(always)]
-    unsafe fn back(&self, mod_index: usize, other: usize) -> f32 {
-        *self
-            .0
-            .get_unchecked(other % SEARCH_RADIUS)
-            .get_unchecked(mod_index)
+    #[derive(Clone, Copy)]
+    pub struct ModIndex(usize);
+
+    impl ModIndex {
+        pub fn new(index: usize) -> Self {
+            Self(index % SEARCH_RADIUS)
+        }
     }
 
-    #[inline(always)]
-    unsafe fn set_front(&mut self, mod_index: usize, other: usize, value: f32) {
-        *self
-            .0
-            .get_unchecked_mut(mod_index)
-            .get_unchecked_mut(other % SEARCH_RADIUS) = value;
+    #[derive(Default)]
+    pub struct FindCache([[f32; SEARCH_RADIUS]; SEARCH_RADIUS]);
+
+    impl FindCache {
+        #[inline(always)]
+        pub fn back(&self, mod_index: ModIndex, other: usize) -> f32 {
+            *unsafe {
+                self.0
+                    .get_unchecked(other % SEARCH_RADIUS)
+                    .get_unchecked(mod_index.0)
+            }
+        }
+
+        #[inline(always)]
+        pub fn set_front(&mut self, mod_index: ModIndex, other: usize, value: f32) {
+            *unsafe {
+                self.0
+                    .get_unchecked_mut(mod_index.0)
+                    .get_unchecked_mut(other % SEARCH_RADIUS)
+            } = value;
+        }
     }
 }
+pub use cache::FindCache;
+use cache::*;
 
 #[inline(always)]
 pub fn find_best_node<Volume: BvhVolume>(
@@ -32,12 +49,11 @@ pub fn find_best_node<Volume: BvhVolume>(
     let mut best_node = index;
     let mut best_area = f32::INFINITY;
 
-    let mod_index = index % SEARCH_RADIUS;
+    let mod_index = ModIndex::new(index);
 
     let begin = index.saturating_sub(SEARCH_RADIUS);
     for other in begin..index {
-        // SAFETY: We pass in the modulo'd index
-        let area = unsafe { cache.back(mod_index, other) };
+        let area = cache.back(mod_index, other);
         if area < best_area {
             best_node = other;
             best_area = area;
@@ -48,10 +64,7 @@ pub fn find_best_node<Volume: BvhVolume>(
     let end = index + SEARCH_RADIUS + 1;
     for (other, node) in nodes.iter().enumerate().take(end).skip(index + 1) {
         let area = our_aabb.merge(&node.volume).visible_area();
-        // SAFETY: We pass in the modulo'd index
-        unsafe {
-            cache.set_front(mod_index, other, area);
-        }
+        cache.set_front(mod_index, other, area);
         if area < best_area {
             best_node = other;
             best_area = area;
